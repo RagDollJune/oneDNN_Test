61330485e6cbc06f53e371ab7f7aa786261ae63f
ragDollJune <juneRagDoll@outlook.com>
2023-09-03 00:17:32 +0800

    remove built output files

--- build/CMakeFiles/dnn_test.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/dnn_test.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/ragdoll/code/hpc/convolution/oneDNN_conv/build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
